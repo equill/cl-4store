62a2fb36c52df2e7166affba0de8ed5fbe829d3d
James Fleming <james@electronic-quill.net>
2012-11-07 20:10:57 +0100

    Export symbols expected to be used externally
    These four, initially:
    - 'sparql-query
    - 'sparql-server-put-data-request
    - 'sparql-server-status-request
    - 'get-triples-list
    
    The latter two are mainly for debugging, so there's a good chance I'll
    deprecate them later.
    
    I may also have imposed some stylistic preferences in the file itself.

--- package.lisp
+++ package.lisp
@@ -1,10 +1,10 @@
-;;; ==============================
-
-
-(defpackage #:4store (:use #:common-lisp )
-            (:import-from #:fare-matcher
-                          #:_))
-
-
-;;; ==============================
-;;; EOF
+(defpackage :4store
+  (:use :common-lisp )
+  (:import-from :fare-matcher
+		:_)
+  (:export sparql-query
+	   sparql-server-put-data-request
+	   ;; Subject to deprecation
+	   ;; - these may not belong here.
+	   sparql-server-status-request
+	   get-triples-list))
