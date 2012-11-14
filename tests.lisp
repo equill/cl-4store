6fad15ff5c39ef819f5fdab412bf700cb0a91f2e
James Fleming <james@electronic-quill.net>
2012-11-14 21:56:18 +0100

    Extend the test suite slightly
    We can now test that the contents of the test graph match the test data,
    that we can nuke the graph, and that the graph is empty when we expect
    it to be.

--- tests.lisp
+++ tests.lisp
@@ -6,13 +6,13 @@
 (fiveam:def-suite 4store-tests)
 (fiveam:in-suite 4store-tests)
 
-(fiveam:test (test-status)
-	     ;; Can we get a sane status reply from the server?
+;; Can we get a sane status reply from the server?
+(fiveam:test (status)
 	     (fiveam:is
 	      (= 200 (4store:sparql-server-status-request *base-uri*))))
 
-(fiveam:test (test-insert-triples)
-	     ;; Can we store a set of triples?
+;; Can we store a set of triples?
+(fiveam:test (insert-triples)
 	     (fiveam:is
 	      (cl-ppcre:scan "200 added successfully"
 		     (4store:insert-triples
@@ -21,4 +21,57 @@
 		      '(("<ox-subjects:foo>" "<ox-predicates:bar>" "plaintext")
 			("<ox-subjects:first-hash>" "<rdfs:label>" "Comment on the primacy of labels")
 			("<ox-subjects:first-hash>" "<rdfs:creator>" "Name of the creator"))))))
+
+;; Do we get the correct set of triples back?
+(fiveam:test (get-triples)
+	     (fiveam:is
+	      (equalp (4store:get-triples-list *base-uri*)
+		     "<?xml version=\"1.0\"?>
+<sparql xmlns=\"http://www.w3.org/2005/sparql-results#\">
+  <head>
+    <variable name=\"subject\"/>
+    <variable name=\"predicate\"/>
+    <variable name=\"object\"/>
+  </head>
+  <results>
+    <result>
+      <binding name=\"subject\"><uri>ox-subjects:foo</uri></binding>
+      <binding name=\"predicate\"><uri>ox-predicates:bar</uri></binding>
+      <binding name=\"object\"><literal>plaintext</literal></binding>
+    </result>
+    <result>
+      <binding name=\"subject\"><uri>ox-subjects:first-hash</uri></binding>
+      <binding name=\"predicate\"><uri>rdfs:label</uri></binding>
+      <binding name=\"object\"><literal>Comment on the primacy of labels</literal></binding>
+    </result>
+    <result>
+      <binding name=\"subject\"><uri>ox-subjects:first-hash</uri></binding>
+      <binding name=\"predicate\"><uri>rdfs:creator</uri></binding>
+      <binding name=\"object\"><literal>Name of the creator</literal></binding>
+    </result>
+  </results>
+</sparql>
+")))
+
+;; Can we nuke the graph?
+(fiveam:test (delete-graph)
+	     (fiveam:is (equal (4store:delete-graph *base-uri* *graph-name*)
+			       "200 deleted successfully
+This is a 4store SPARQL server v1.1.5-27-gc3d8593
+")))
+
+;; Is the graph empty?
+(fiveam:test (empty-graph)
+	     (fiveam:is (equal (4store:get-triples-list *base-uri*)
+			       "<?xml version=\"1.0\"?>
+<sparql xmlns=\"http://www.w3.org/2005/sparql-results#\">
+  <head>
+    <variable name=\"subject\"/>
+    <variable name=\"predicate\"/>
+    <variable name=\"object\"/>
+  </head>
+  <results>
+  </results>
+</sparql>
+")))
 
