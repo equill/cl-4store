227a5d5d2ebe0d47bba931f04ce4fdd581ca57fa
James Fleming <james@electronic-quill.net>
2012-11-17 19:20:09 +0100

    Move two more FOAF-related chunks to foaf.lisp

--- 4store-specials.lisp
+++ 4store-specials.lisp
@@ -13,14 +13,3 @@
 
 (defvar *4store-base-url* "http://localhost:8080/")
 
-(defvar *4store-query-cache* (make-hash-table))
-
-(setf (gethash :rdfs-class-select *4store-query-cache*)
-"prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-select distinct ?type 
-where { 
-    ?x a ?type .
-} 
-order by ?type")
-
--- foaf.lisp
+++ foaf.lisp
@@ -8,6 +8,17 @@
 ;:cl-rdfxml
 ;:fare-matcher
 
+(defvar *4store-query-cache* (make-hash-table))
+
+(setf (gethash :rdfs-class-select *4store-query-cache*)
+"prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+select distinct ?type 
+where { 
+    ?x a ?type .
+} 
+order by ?type")
+
 (setf (gethash :foaf-person-construct *4store-query-cache*)
       "prefix foaf: <http://xmlns.com/foaf/0.1/>
 construct {
