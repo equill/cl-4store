89aa76326a363bacf2d896eebbb02af2894a1dc2
James Fleming <james@electronic-quill.net>
2012-11-17 19:14:39 +0100

    Add "text/plain" to the supported return types
    Just for completeness, as it's not actually blocking anything right now.

--- 4store-specials.lisp
+++ 4store-specials.lisp
@@ -5,9 +5,10 @@
 ;; binary.
 (defvar *4store-text-content-types* 
   (list* '("application". "sparql-results+xml")
-	 '("text" . "tab-separated-values")
-	 '("application" . "sparql-results+json")
+         '("text" . "tab-separated-values")
+         '("application" . "sparql-results+json")
          '("application" . "rdf+xml")
+         '("text" . "plain")
          drakma:*text-content-types*))
 
 (defvar *4store-base-url* "http://localhost:8080/")
